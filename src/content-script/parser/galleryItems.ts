c89102b0be1365d0015bcffa765999235b06c836
fesmofet <borees2@gmail.com>
2023-07-03 14:44:56 +0300

    gallery issue

--- src/content-script/parser/galleryItems.ts
+++ src/content-script/parser/galleryItems.ts
@@ -1,16 +1,14 @@
-import { GALLERY_SELECTOR } from '../constants';
-
 export const getGalleryItems = (): string[] => {
   const altImages = Array.from(
-    document.querySelectorAll<HTMLImageElement>(GALLERY_SELECTOR.ALT_IMAGES),
+    document.querySelectorAll<HTMLElement>('#altImages li.a-spacing-small.item.imageThumbnail.a-declarative'),
   );
   for (const altImage of altImages) {
     altImage.click();
   }
-  const images = Array.from(document.querySelectorAll<HTMLImageElement>(GALLERY_SELECTOR.MAIN))
+  const images = Array.from(document.querySelectorAll<HTMLImageElement>('li:not([data-csa-c-posy="1"]) .imgTagWrapper img'))
     .map((img) => img.src)
     .filter((el) => !!el)
-    .slice(1);
+    .filter((el, index, self) => index === self.indexOf(el));
 
   return images;
 };
