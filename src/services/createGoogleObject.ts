65acc92a453359151f7f8db340014ee6ade3db74
fesmofet <borees2@gmail.com>
2024-02-20 15:04:07 +0200

    remove address

--- src/services/createGoogleObject.ts
+++ src/services/createGoogleObject.ts
@@ -151,26 +151,10 @@ export const getGooglePlace = async (textQuery: string) => {
   }
   const [business] = result;
 
-  const city = business.addressComponents
-    .find((el) => el.types.every((t) => ['locality', 'political'].includes(t)));
-
-  const country = business.addressComponents
-    .find((el) => el.types.every((t) => ['country', 'political'].includes(t)));
-  const province = business.addressComponents
-    .find((el) => el.types
-      .every((t) => ['administrative_area_level_1', 'political'].includes(t)));
-
-  const postCode = business.addressComponents
-    .find((el) => el.types.every((t) => ['postal_code'].includes(t)));
-
   const objectData = {
     name: business.displayName.text,
     address: business.formattedAddress,
     ...(business.editorialSummary && { descriptions: [business.editorialSummary.text] }),
-    ...(city && { city: city.longText }),
-    ...(province && { province: province.longText }),
-    ...(postCode && { postalCode: postCode.longText }),
-    ...(country && { country: country.longText }),
     ...(business.location
         && { latitude: business.location.latitude, longitude: business.location.longitude }),
     ...(business.regularOpeningHours && { workingHours: business.regularOpeningHours.weekdayDescriptions.join(',\n') }),
