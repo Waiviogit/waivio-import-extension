ec9ed61ac937885cf4e0a563a73d964acfd55d3d
fesmofet <borees2@gmail.com>
2025-01-30 13:36:13 +0200

    instagram description fix

--- src/content-script/helpers/instaHelper.ts
+++ src/content-script/helpers/instaHelper.ts
@@ -1,4 +1,10 @@
 export const getInstagramUsername = (): string => document.querySelector<HTMLElement>('span span div a')?.innerText
         || '';
 
-export const getInstagramDescription = (): string => document.querySelector<HTMLElement>('h1')?.innerText || '';
+export const getInstagramDescription = (): string => {
+  const items = Array.from(document.querySelectorAll<HTMLElement>('h1'));
+  for (const item of items) {
+    if (item.innerText) return item.innerText;
+  }
+  return '';
+};
