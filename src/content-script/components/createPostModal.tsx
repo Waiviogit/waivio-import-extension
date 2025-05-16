f755566606f2f4406c6fc0e7d03bcb21e1af0c7c
fesmofet <borees2@gmail.com>
2025-05-16 11:42:37 +0300

    modal fix

--- src/content-script/components/createPostModal.tsx
+++ src/content-script/components/createPostModal.tsx
@@ -1,9 +1,8 @@
 import React, { useState } from 'react';
 import {
-  ConfigProvider, Modal, Input, Popover, Button, Tooltip,
+  ConfigProvider, Input, Popover, Button, Tooltip,
 } from 'antd';
 import { ReloadOutlined, VideoCameraAddOutlined } from '@ant-design/icons';
-import Draggable, { DraggableEvent, DraggableData } from 'react-draggable';
 import WaivioTags from './WaivioTags';
 import { postImportWaivio } from '../helpers/downloadWaivioHelper';
 import { copyContent } from '../helpers/commonHelper';
@@ -13,6 +12,7 @@ import { getDraftBodyTitleTags } from '../helpers/draftHelper';
 import { extractPostInfo } from '../helpers/postHelper';
 import { createAnalysisVideoPromptBySource } from '../helpers/promptHelper';
 import { videoAnalysesByLink } from '../helpers/gptHelper';
+import { DraggableModal } from './DraggableModal';
 
 interface CreatePostProps {
     author: string;
@@ -31,11 +31,6 @@ const RECIPE_SOURCES = [
   SOURCE_TYPES.RECIPE_DRAFT_INSTAGRAM,
 ];
 
-const POST_SOURCES = [
-  SOURCE_TYPES.YOUTUBE,
-  SOURCE_TYPES.INSTAGRAM,
-  SOURCE_TYPES.TIKTOK,
-];
 
 const CreatePostModal = ({
   author, body: initialBody, title: initialTitle = 'Post draft', tags, host, source,
@@ -44,28 +39,9 @@ const CreatePostModal = ({
   const [isRecipeLoading, setIsRecipeLoading] = useState(false);
   const [isRefreshLoading, setIsRefreshLoading] = useState(false);
   const [isAnalysisLoading, setIsAnalysisLoading] = useState(false);
-  const [disabled, setDisabled] = useState(true);
-  const [bounds, setBounds] = useState({
-    left: 0, top: 0, bottom: 0, right: 0,
-  });
   const [title, setTitle] = useState(initialTitle);
   const [body, setBody] = useState(initialBody);
   const [postTags, setTags] = useState<string[]>([]);
-
-  const draggleRef = React.useRef<HTMLDivElement>(null);
-
-  const onStart = (event: DraggableEvent, uiData: DraggableData) => {
-    const { clientWidth, clientHeight } = window.document.documentElement;
-    const targetRect = draggleRef.current?.getBoundingClientRect();
-    if (!targetRect) return;
-
-    setBounds({
-      left: -targetRect.left + uiData.x,
-      right: clientWidth - (targetRect.right - uiData.x),
-      top: -targetRect.top + uiData.y,
-      bottom: clientHeight - (targetRect.bottom - uiData.y),
-    });
-  };
 
   const handleSubmit = async () => {
     const nested = document.getElementById('react-chrome-modal');
@@ -130,9 +106,6 @@ const CreatePostModal = ({
   };
 
   const recipeButtonExist = RECIPE_SOURCES.includes(source || '');
-  const isDraft = !POST_SOURCES.includes(source || '');
-
-  const submitDisabled = !title || !body || title?.length > MAX_TITLE_LENGTH;
 
   const recipeButtons = <>
       <Button
@@ -141,7 +114,6 @@ const CreatePostModal = ({
   >
       Create object
   </Button>
-
       </>;
 
   const draftButtons = <>
@@ -161,107 +133,70 @@ const CreatePostModal = ({
           </Tooltip>
       </>;
 
+  const footerComponents = (
+    <>
+      {recipeButtonExist && recipeButtons}
+      {draftButtons}
+      <Button onClick={handleCopy}>Copy</Button>
+    </>
+  );
+
   return (
-        <>
-            <ConfigProvider
-                theme={{
-                  token: {
-                    colorPrimary: '#f87007',
-                  },
-                }}
-            >
-                <Modal
-                    title={
-                      <div
-                        style={{
-                          width: '100%',
-                          cursor: 'move',
-                        }}
-                        onMouseOver={() => {
-                          if (disabled) {
-                            setDisabled(false);
-                          }
-                        }}
-                        onMouseOut={() => {
-                          setDisabled(true);
-                        }}
-                        onFocus={() => undefined}
-                        onBlur={() => undefined}
-                      >
-                        Create Post
-                      </div>
-                    }
-                    modalRender={(modal) => (
-                      <Draggable
-                        disabled={disabled}
-                        bounds={bounds}
-                        onStart={(event, uiData) => onStart(event, uiData)}
-                      >
-                        <div ref={draggleRef}>{modal}</div>
-                      </Draggable>
-                    )}
-                    bodyStyle={{ overflowY: 'auto', maxHeight: 'calc(100vh - 200px)' }}
-                    open={isModalOpen}
-                    onOk={handleSubmit}
-                    onCancel={handleCancel}
-                    okText="Publish"
-                    cancelText="Cancel"
-                    okButtonProps={{ disabled: submitDisabled }}
-                    zIndex={9999999}
-                    maskClosable={false}
-                    footer={(_, { OkBtn, CancelBtn }) => (
-                        <>
-                            {recipeButtonExist && recipeButtons}
-                            {draftButtons}
-                            <Button
-                                onClick={handleCopy}
-                            >
-                                Copy
-                            </Button>
-                            <CancelBtn />
-                            <OkBtn />
-                        </>
-                    )}
-                >
-                    <Popover
-                        content={'Author of the post. Change by signing in to Waivio with different account.'}
-                    >
-                        <Input
-                            style={{ marginTop: '30px' }}
-                            value={author}
-                            disabled={true}
-                        />
-                    </Popover>
-                    <Popover
-                        content={'Domain associated with a post, establishing it as its canonical source. Can be changed on Waivio.'}
-                    >
-                        <Input
-                            value={host}
-                            disabled={true}
-                            style={{ marginTop: '10px' }}
-                        />
-                    </Popover>
-                    <Input
-                        value={title}
-                        showCount
-                        maxLength={MAX_TITLE_LENGTH}
-                        onChange={(e) => setTitle(e.target.value)}
-                        placeholder="Enter title"
-                        style={{ marginTop: '10px' }}
-                    />
-                    <Input.TextArea
-                        value={body}
-                        onChange={(e) => setBody(e.target.value)}
-                        placeholder="Enter post content"
-                        style={{ height: '400px', marginTop: '10px', marginBottom: '10px' }}
-                    />
-                    <WaivioTags
-                        setParentTags={setTags}
-                        initialTags={tags}
-                    />
-                </Modal>
-            </ConfigProvider>
-        </>
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: '#f87007',
+        },
+      }}
+    >
+      <DraggableModal
+        title="Create Post"
+        open={isModalOpen}
+        onOk={handleSubmit}
+        onCancel={handleCancel}
+        okText="Publish"
+        cancelText="Cancel"
+        footerComponents={footerComponents}
+        width={500}
+      >
+        <Popover
+          content={'Author of the post. Change by signing in to Waivio with different account.'}
+        >
+          <Input
+            style={{ marginTop: '30px' }}
+            value={author}
+            disabled={true}
+          />
+        </Popover>
+        <Popover
+          content={'Domain associated with a post, establishing it as its canonical source. Can be changed on Waivio.'}
+        >
+          <Input
+            value={host}
+            disabled={true}
+            style={{ marginTop: '10px' }}
+          />
+        </Popover>
+        <Input
+          value={title}
+          showCount
+          maxLength={MAX_TITLE_LENGTH}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Enter title"
+          style={{ marginTop: '10px' }}
+        />
+        <Input.TextArea
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+          placeholder="Enter post content"
+          style={{ height: '400px', marginTop: '10px', marginBottom: '10px' }}
+        />
+        <WaivioTags
+          setParentTags={setTags}
+          initialTags={tags}
+        />
+      </DraggableModal>
+    </ConfigProvider>
   );
 };
 
