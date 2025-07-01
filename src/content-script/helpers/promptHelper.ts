ca91095958a98f8bc7626a04c8179948a5010f9f
fesmofet <borees2@gmail.com>
2025-07-01 15:04:54 +0300

    issues/7627

--- src/content-script/helpers/promptHelper.ts
+++ src/content-script/helpers/promptHelper.ts
@@ -1,6 +1,13 @@
 import { RECIPE_SOURCE_TYPES } from '../../common/constants';
 
-const recipeVideoPrompt = `Analyze the provided cooking video thoroughly and extract as much information as possible the following. Include the following details:
+const recipeVideoPrompt = (content = '') => `
+You are given the video **title and description** below, which may provide useful context about the video’s content:
+---
+${content}
+---
+
+Analyze the provided cooking video thoroughly and extract as much information as possible the following. Include the following details:
+
 
 1. Recipe Overview:
 Brief description or gist of the recipe.
@@ -10,21 +17,24 @@ Type of cuisine and dish category (e.g., appetizer, main course, dessert).
 2. Key Ingredients:
 List all the main ingredients mentioned, including quantities and measurements.
 
-3. Preparation Time:
+3. Nutrition facts per serving:
+Calories, Proteins, fats and carbohydrates per serving.
+
+4. Preparation Time:
 Total preparation time mentioned or inferred from the video.
 
 Cooking and resting times separately, if applicable.
 
-4. Cooking Instructions:
+5. Cooking Instructions:
 Provide step-by-step cooking instructions in chronological order, clearly indicating each action taken.
 
-5. Special Techniques or Tips:
+6. Special Techniques or Tips:
 Highlight any special cooking techniques, chef's tips, or important notes mentioned in the video.
 
-6. Serving Suggestions:
+7. Serving Suggestions:
 Describe recommended serving methods, garnishes, side dishes, or beverages suggested.
 
-7. Visual Observations:
+8. Visual Observations:
 Note any important visual cues, such as color or texture changes indicating the readiness of the dish.
 
 Provide the information clearly structured and organized under each heading. 
@@ -64,6 +74,6 @@ Analyze the video and provide the following:
 `;
 
 export const createAnalysisVideoPromptBySource = (source:string, content: string): string => {
-  if (RECIPE_SOURCE_TYPES.includes(source)) return recipeVideoPrompt;
+  if (RECIPE_SOURCE_TYPES.includes(source)) return recipeVideoPrompt(content);
   return regularVideoPrompt(content);
 };
